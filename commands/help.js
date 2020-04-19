ed0eb1e68c036b0e214d7fb07ed56c1c324a0327
Glitch (polar-fluorescent-carp) <none>
2020-04-19 10:37:17 +0000

    🐣⛵️ Checkpoint ./commands/help.js:1401279/153

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,12 @@
 exports.run = function (client, msg, args) {
     
-  let commands = client.commands.map(x => `\`${x.name}\``).join(", ");
+  let commands = Array.from(client.commands.keys()).sort().join(', ');
   console.log(commands);
   
     msg.channel.createMessage({embed: {
         color: 0xFFD100,
         title: `Commands for ${client.user.username}`,
-        description: commands
+        description: `\`${commands}\``
     }});
 };
 
