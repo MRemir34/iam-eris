3587c673d104c68c97744e4b73be95ddde36419d
Glitch (polar-fluorescent-carp) <none>
2020-04-19 14:20:19 +0000

    🍢🐉 Checkpoint ./commands/avatar.js:1401279/244

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,8 @@
 exports.run = function (client, msg, args) {
     
-   let user = msg.mentions.users.first() || client.users.get(args[0])
-   if(user) user = msg.author;
+   let user = client.users.get(args[0]);
+   console.log(msg.mentions[0])
+   if(!user) user = msg.author;
   
     msg.channel.createMessage({embed: {
         color: client.config.colors.success,
