9c0dcc92a015d4c3224d707e79dc57949f3a5f81
Glitch (polar-fluorescent-carp) <none>
2020-04-19 08:14:16 +0000

    🍀🏪 Checkpoint ./commands/stats.js:1401279/488

--- commands/stats.js
+++ commands/stats.js
@@ -1,18 +1,17 @@
 exports.run = async function (client, msg, args) {
-    
-    let findOwner = client.config.ownerID.shift(); //to get your owner name put your ID at index 0 in config
-  
-    let getOwner = client.users.get(findOwner);
-   // console.log(getOwner)
-    let owner = `${getOwner.username}#${getOwner.discriminator}`;
   
     let wsPING = client.shards.get(0).latency;
+  
+    let findOwner = client.config.ownerID[0]; //to get your owner name put your ID at index 0 in config
+  
+    let getOwner = client.users.get(findOwner);
 
+    let owner = `${getOwner.username}#${getOwner.discriminator}`;
   
     msg.channel.createMessage({embed: {
         color: 0xFFD100,
         description: `
-• Created By: ${client.util.codeBlock(''+getOwner.discriminator)}
+• Created By: ${client.util.codeBlock(owner)}
 • PING: ${client.util.codeBlock(wsPING+'ms')}
 • UPTIME: ${client.util.codeBlock(client.util.timeParser(client.uptime))}
 • Total Guilds: ${client.util.codeBlock(client.guilds.size)}
