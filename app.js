cf2a1e7489b454f36a96be185c94799dfa6a1bf8
Glitch (polar-fluorescent-carp) <none>
2020-04-20 13:38:17 +0000

    ☂️🌘 Checkpoint ./app.js:1401279/33 ./commands/avatar.js:1401279/8 ./README.md:1401279/19 ./package.json:1401279/162

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const ErisClient = require('./structures/Client.js');
 const client = new ErisClient(require('./config.js'), {
 maxShards: 'auto',
 messageLimit: 0,
-getAllUsers: false
+getAllUsers: false,
+disableEveryone: true
 });
 
 client.connect();
