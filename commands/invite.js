896b58f7a43ba29e01703477a6774b15c0589162
Glitch (polar-fluorescent-carp) <none>
2020-04-21 06:53:06 +0000

    😋🏑 Checkpoint ./commands/invite.js:1401279/938 ./commands/serverinfo.js:1401279/691 ./structures/util.js:1401279/238

---
+++ commands/invite.js
@@ -0,0 +1,11 @@
+exports.run = function (client, msg, args) {
+    
+    let link = `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`;
+  
+    msg.channel.createMessage({embed: {
+        color: client.config.colors.success,
+        description: `Add me to you [server](${link}) || Join my support [server](https://discord.gg/X9r96Ee)`
+    }});
+};
+
+exports.aliases = ["support"];
--- structures/util.js
+++ structures/util.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const months = ["January", "February", "March", "April", "May", "June", "July",
+	"August", "September", "October", "November", "December"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 
 /**
  * Utilities module
