3c4b1854622712dcff1af6814d858195962efe80
Glitch (polar-fluorescent-carp) <none>
2020-04-21 06:06:05 +0000

    🎲🎰 Checkpoint ./commands/eval.js:1401279/12436

--- commands/eval.js
+++ commands/eval.js
@@ -3,72 +3,33 @@ exports.run = async (client, msg, args) => {
     
     if (msg.author.id !== owner) return;
 
-  const {
-            args,
-            flag
-        } = msg;
+     try {
+        let code = eval(args.join(' '));
 
-        try {
-            const code = args.join(" ");
-            if (!code) return;
-            let evaled;
-
-            if (flag.includes("async")) evaled = await eval(`(async () => { ${code} })()`);
-            else evaled = eval(code);
-
-            if (flag.includes("silent")) return;
-
-            if (typeof evaled !== "string") {
-                evaled = require("util").inspect(evaled, {
-                    depth: 0
-                });
-                evaled = evaled.replace(client.token, "[TOKEN]");
-            }
-            const output = clean(evaled);
-            let result;
-            if (output.length > 2000) {
-                const ot = await client.util.haste(output);
-                result = ot;
-            } else result = output;
-            const isURL = validateURL(result);
-            msg.channel.createMessage({
-            embed: {
-            description: isURL ? result : client.util.codeBlock(result, "js")
-            
-            }});
-           } catch (e) {
-            const error = clean(e);
-            let result;
-            if (error.length > 2000) {
-                const ot = await client.util.haste(error);
-                result = ot;
-            } else result = error;
-            const isURL = validateURL(result);
-            msg.channel.createMessage({
-            embed: {
-            description: isURL ? result : client.util.codeBlock(result, "js")
-            
-            }});
+        if (typeof code !== 'string') {
+            code = require('util').inspect(code, { depth: 0 });
         }
+        let output = client.util.clean(code);
+        output = output.replace(new RegExp(client.token.slice(3), 'gi'), 'GG! You hungry?');
+       if(code.length > 1023) {
+       let res = await client.util.haste(output);
+       msg.channel.createMessage({embed:{
+        color: client.config.colors.success,
+        description: res
+      }});
+       } else {
+        msg.channel.createMessage({embed:{
+        color: client.config.colors.success,
+        description: client.util.codeBlock(output, "js"),
+        fields: [
+        { name: "Type", value: client.util.codeBlock(typeof output) } 
+        ]
+        }});
+       }
+    } catch(e) {
+        msg.channel.createMessage({embed: {color: client.config.colors.error, description: client.util.codeBlock(e)}});
+    }
   });
 }
-
-   function clean(text) {
-        if (typeof text === "string")
-            return text
-                .replace(/`/g, "`" + String.fromCharCode(8203))
-                .replace(/@/g, "@" + String.fromCharCode(8203));
-        else return text;
-    }
-
-   function validateURL(str) {
-        const pattern = new RegExp("^(https?:\\/\\/)?" + // protocol
-            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-            "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-            "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-            "(\\#[-a-z\\d_]*)?$", "i"); // fragment locator
-        return !!pattern.test(str);
-    }
 
 exports.aliases = ['ev']
