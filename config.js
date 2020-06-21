d1154e694ff7e6830e7131d8ba5625039f55eda8
Glitch (polar-fluorescent-carp) <none>
2020-06-21 19:29:36 +0000

    🎙🦄 Checkpoint ./config.js:14982120/36 ./events/ready.js:14982120/66

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
      "TOKEN" : process.env.TOKEN,
      "PREFIX" : "e!",
-     "ownerID": ["525322591796199445"],
+     "ownerID": ["453568928669630465"],
      "colors": {
         "error": 0xFF0000,
         "success": 0x00FF46,
